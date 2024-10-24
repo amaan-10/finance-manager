194dfd3a02e23e057fa22773fc052d3944c22271
Amaan <amaanshaikh.gg@gmail.com>
2024-10-24 19:35:28 +0530

    site updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,28 @@
-import { SignedIn, SignedOut, SignInButton, SignUp, SignUpButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div> 
+    <div>
       <div className=" sign-in-container">
         <SignedOut>
-          <SignInButton mode='modal' />
-          <SignUpButton mode="modal"/>
+          <SignInButton mode="modal" />
+          <SignUpButton mode="modal" />
         </SignedOut>
       </div>
-      
+
       <SignedIn>
         <UserButton />
       </SignedIn>
       <section>
-      <h2>Welcome to Personal Finance Manager</h2>
-      <p>Track your expenses, create budgets, and manage your investments.</p>
-    </section>
-      
+        <h2>Welcome to Personal Finance Manager</h2>
+        <p>Track your expenses, create budgets, and manage your investments.</p>
+      </section>
     </div>
   );
 }
