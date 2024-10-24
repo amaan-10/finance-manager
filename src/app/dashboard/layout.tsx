23ee767dc2d894be7bd44df8104c838e068ee2dc
Amaan <amaanshaikh.gg@gmail.com>
2024-10-24 21:55:56 +0530

    site updated

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,23 @@
 // app/dashboard/layout.tsx
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <div>
-        <nav>
-          <a href="/dashboard">Overview</a>
-          <a href="/dashboard/expenses">Expenses</a>
-          <a href="/dashboard/investments">Investments</a>
-        </nav>
-        <div>{children}</div>
-      </div>
-    );
-  }
-  
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <nav>
+        <a className=" text-black" href="/dashboard">
+          Overview
+        </a>
+        <a className=" text-black" href="/dashboard/expenses">
+          Expenses
+        </a>
+        <a className=" text-black" href="/dashboard/investments">
+          Investments
+        </a>
+      </nav>
+      <div>{children}</div>
+    </div>
+  );
+}
