fa43bad26ffc26698ecf4c6cfedee6186282f7e2
Amaan <amaanshaikh.gg@gmail.com>
2024-10-26 00:13:21 +0530

    Investments Page Created

---
+++ src/app/dashboard/investments/page.tsx
@@ -0,0 +1,9 @@
+// app/investments/page.tsx
+export default function InvestmentsPage() {
+  return (
+    <section className=" mt-5">
+      <h2>Investments Overview</h2>
+      <h2 className=" bg-yellow-300">Coming Soon...</h2>
+    </section>
+  );
+}
