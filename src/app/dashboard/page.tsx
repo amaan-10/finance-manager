e0182001ddd26e20f7bb159663907c978f1058f8
Amaan <amaanshaikh.gg@gmail.com>
2024-10-27 20:22:15 +0530

    modified

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,37 @@
+"use client";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+
+interface Expense {
+  totalAmount: number;
+  _id: {
+    month: number;
+    year: number;
+  };
+}
 
 // app/dashboard/page.tsx
 export default function DashboardPage() {
+  const [expenses, setExpenses] = useState<Expense[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchMonthlyExpenses = async () => {
+      try {
+        const res = await fetch(`/api/expenses/monthly`);
+        const data = await res.json();
+        console.log("Monthly Expenses:", data);
+        setExpenses(data);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchMonthlyExpenses();
+  }, []);
+
   return (
     <section className=" mt-5">
       <h2>Dashboard Overview</h2>
@@ -9,13 +39,31 @@ export default function DashboardPage() {
       <h2 className=" bg-yellow-300">Work in Progress...</h2>
       <div className="mt-2">
         <div>
-          Monthly Expenses: <span className=" font-serif">₹</span>500
+          Monthly Expenses:
+          {loading ? (
+            <p>Loading expenses...</p> // Loading state
+          ) : error ? (
+            <p>Error: {error}</p> // Error state
+          ) : !expenses || expenses.length === 0 ? (
+            <p>No expenses found for this user.</p> // No data found
+          ) : (
+            // Render expense list if data exists
+            <>
+              {expenses.map((expense, index) => (
+                <li key={index}>
+                  {expense._id.month}, {expense._id.year} -{" "}
+                  <span className=" font-serif">₹</span>
+                  {expense.totalAmount}
+                </li>
+              ))}
+            </>
+          )}
         </div>
         <div>
           Remaining Budget: <span className=" font-serif">₹</span>200
         </div>
         <div>
-          Total Investments: <span className=" font-serif">₹</span>1500
+          Total Investments: <span className=" font-serif">₹</span>--
         </div>
       </div>
       <nav className="mt-5">
