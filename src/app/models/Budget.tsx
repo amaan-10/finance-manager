54a6e54527a970a17fd85a92f1cc26dbe18fa2ba
Amaan <amaanshaikh.gg@gmail.com>
2024-10-28 20:03:50 +0530

    modified

--- src/app/models/Budget.tsx
+++ src/app/models/Budget.tsx
@@ -1,18 +1,20 @@
 // models/Budget.ts
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, models, Document } from "mongoose";
 
 export interface Budget extends Document {
-  category: string;
   amount: number;
-  month: string; // You can also use a Date
+  month: number; // You can also use a Date
+  year: number;
+  userId: string; // Add userId to associate with the user
 }
 
 const budgetSchema = new Schema<Budget>({
-  category: { type: String, required: true },
+  userId: { type: String, required: true },
   amount: { type: Number, required: true },
-  month: { type: String, required: true }, // e.g., '2024-10'
+  month: { type: Number, required: true }, // e.g., '10'
+  year: { type: Number, required: true }, // e.g., '2024'
 });
 
-const BudgetModel = model<Budget>("Budget", budgetSchema);
+const BudgetModel = models.Budget || model<Budget>("Budget", budgetSchema);
 
 export default BudgetModel;
