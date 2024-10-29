7593d7b79f4bc47f265d836eb2a65df9423bbd11
Amaan <amaanshaikh.gg@gmail.com>
2024-10-29 23:54:50 +0530

    delete budget func updated

--- src/app/api/budgets/[month]/[year]/route.tsx
+++ src/app/api/budgets/[month]/[year]/route.tsx
@@ -1,52 +1,53 @@
-import BudgetModel from "@/app/models/Budget";
-import clientPromise from "@/lib/mongodb";
-import { connectToDatabase } from "@/lib/mongoose";
-import { getAuth } from "@clerk/nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
+// import BudgetModel from "@/app/models/Budget";
+// import clientPromise from "@/lib/mongodb";
+// import { connectToDatabase } from "@/lib/mongoose";
+// import { getAuth } from "@clerk/nextjs/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { month: string; year: string } }
-) {
-  try {
-    await connectToDatabase(); // Ensure MongoDB is connected
+// export async function DELETE(
+//   req: NextRequest
+// ) {
+//   try {
+//     await connectToDatabase(); // Ensure MongoDB is connected
 
-    const { month, year } = params;
-    await clientPromise;
-    const client = await clientPromise;
-    const db = client.db();
+//     const { searchParams } = new URL(req.url);
+//     const month = searchParams.get("month");
+//     const year = searchParams.get("year");
+//     await clientPromise;
+//     const client = await clientPromise;
+//     const db = client.db();
 
-    const { userId } = getAuth(req); // Get the authenticated user ID
-    //const { amount, month, year } = await req.json();
-    // console.log(
-    //   "request------------------------------------------------------------",
-    //   month,
-    //   year
-    // );
+//     const { userId } = getAuth(req); // Get the authenticated user ID
+//     //const { amount, month, year } = await req.json();
+//     // console.log(
+//     //   "request------------------------------------------------------------",
+//     //   month,
+//     //   year
+//     // );
 
-    // Find and delete the budget by ID
-    const deletedBudget = await BudgetModel.findOneAndDelete({
-      userId,
-      month,
-      year,
-    });
+//     // Find and delete the budget by ID
+//     const deletedBudget = await BudgetModel.findOneAndDelete({
+//       userId,
+//       month,
+//       year,
+//     });
 
-    if (!deletedBudget) {
-      return NextResponse.json(
-        { message: "Budget not found" },
-        { status: 404 }
-      );
-    }
+//     if (!deletedBudget) {
+//       return NextResponse.json(
+//         { message: "Budget not found" },
+//         { status: 404 }
+//       );
+//     }
 
-    return NextResponse.json({
-      message: "Budget deleted successfully",
-      deletedBudget,
-    });
-  } catch (error) {
-    console.error("Error deleting budget:", error);
-    return NextResponse.json(
-      { error: "Failed to delete budget" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({
+//       message: "Budget deleted successfully",
+//       deletedBudget,
+//     });
+//   } catch (error) {
+//     console.error("Error deleting budget:", error);
+//     return NextResponse.json(
+//       { error: "Failed to delete budget" },
+//       { status: 500 }
+//     );
+//   }
+// }
