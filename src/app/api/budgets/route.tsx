76d9a0ce0e6581e553e6d7ac9e8ffefba8677538
Amaan <amaanshaikh.gg@gmail.com>
2024-10-29 23:31:59 +0530

    update budget func added

--- src/app/api/budgets/route.tsx
+++ src/app/api/budgets/route.tsx
@@ -21,12 +21,73 @@ export async function POST(req: NextRequest) {
   await connectToDatabase(); // Ensure MongoDB is connected
 
   const { userId } = getAuth(req); // Get the authenticated user ID
-  console.log(userId);
+  //console.log(userId);
   const client = await clientPromise;
-  const body = await req.json();
+  const { amount, month, year } = await req.json();
 
-  const budget = new BudgetModel({ ...body, userId });
+  // Check if budget already exists for the user and month
+  const existingBudget = await BudgetModel.findOne({ userId, month, year });
+
+  if (existingBudget) {
+    const updatedBudget = await BudgetModel.findOneAndUpdate(
+      { userId, month, year },
+      { amount },
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedBudget) {
+      return NextResponse.json(
+        { message: "Budget not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      message: "Budget updated successfully",
+      updatedBudget,
+    });
+  }
+
+  //console.log(body);
+  const budget = new BudgetModel({ amount, month, year, userId });
   await budget.save();
 
-  return NextResponse.json(budget);
+  return NextResponse.json({ message: "Budget set successfully", budget });
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    await connectToDatabase(); // Ensure MongoDB is connected
+
+    const { userId } = getAuth(req); // Get the authenticated user ID
+    //console.log(userId);
+    const client = await clientPromise;
+    const { amount, month, year } = await req.json();
+
+    // Find the budget by ID and update it
+    const updatedBudget = await BudgetModel.findByIdAndUpdate(
+      userId,
+      { amount, month, year, $inc: { __v: 1 } },
+
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedBudget) {
+      return NextResponse.json(
+        { message: "Budget not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      message: "Budget updated successfully",
+      updatedBudget,
+    });
+  } catch (error) {
+    console.error("Error updating budget:", error);
+    return NextResponse.json(
+      { error: "Failed to update budget" },
+      { status: 500 }
+    );
+  }
 }
