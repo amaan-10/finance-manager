0d0b4469a22475bbb096b20ba1f01fb436df36fa
Amaan <amaanshaikh.gg@gmail.com>
2024-10-30 16:05:08 +0530

    expense delete func updated

--- src/app/api/expenses/route.tsx
+++ src/app/api/expenses/route.tsx
@@ -18,7 +18,10 @@ export async function POST(req: NextRequest) {
     console.log(expense);
     await expense.save();
 
-    return NextResponse.json(expense);
+    return NextResponse.json({
+      message: "Expense added successfully",
+      expense,
+    });
   } catch (error) {
     console.error("Error saving expense:", error);
     return NextResponse.json(
@@ -39,4 +42,57 @@ export async function GET(req: NextRequest) {
     .find({ userId })
     .toArray(); // Filter by userId
   return NextResponse.json(expenses);
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    await connectToDatabase(); // Ensure MongoDB is connected
+
+    const body = await req.json(); // Get the request body
+    const { category, amount, date } = body; // Destructure month and year
+
+    if (!category || !amount || !date) {
+      return NextResponse.json(
+        { message: "Month and year are required." },
+        { status: 400 }
+      );
+    }
+    await clientPromise;
+    const client = await clientPromise;
+    const db = client.db();
+
+    const { userId } = getAuth(req); // Get the authenticated user ID
+    //const { amount, month, year } = await req.json();
+    // console.log(
+    //   "request------------------------------------------------------------",
+    //   month,
+    //   year
+    // );
+
+    // Find and delete the budget by ID
+    const deletedExpense = await ExpenseModel.findOneAndDelete({
+      userId,
+      category,
+      amount,
+      date,
+    });
+
+    if (!deletedExpense) {
+      return NextResponse.json(
+        { message: "Expense not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      message: "Expense deleted successfully",
+      deletedExpense,
+    });
+  } catch (error) {
+    console.error("Error deleting expense:", error);
+    return NextResponse.json(
+      { error: "Failed to delete expense" },
+      { status: 500 }
+    );
+  }
 }
