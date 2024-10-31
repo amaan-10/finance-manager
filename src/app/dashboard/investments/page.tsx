b19388fd8f2ec6f0ffb3ef72e48fa1a15bc8b486
Amaan <amaanshaikh.gg@gmail.com>
2024-10-31 23:33:07 +0530

    stocks tracking added

--- src/app/dashboard/investments/page.tsx
+++ src/app/dashboard/investments/page.tsx
@@ -1,5 +1,6 @@
 // app/investments/page.tsx
 "use client";
+import Link from "next/link";
 import React, { useState, useEffect } from "react";
 
 interface Investment {
@@ -11,9 +12,27 @@ interface Investment {
   name?: string;
 }
 
+interface StockData {
+  name?: string;
+  amount: number;
+  inv_date: string;
+  status: string;
+  c: number; // Current price
+  d: number;
+  dp: number;
+  h: number; // High price of the day
+  l: number; // Low price of the day
+  o: number; // Opening price
+  pc: number; // Previous close
+  t: number;
+}
+
 const InvestmentsPage: React.FC = () => {
   const [investments, setInvestments] = useState<Investment[]>([]);
+  //const [stockData, setStockData] = useState<StockData | null>(null);
+  const [stockData, setStocks] = useState<StockData[]>([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   const [type, setType] = useState("");
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
@@ -56,6 +75,30 @@ const InvestmentsPage: React.FC = () => {
     }
   }
 
+  useEffect(() => {
+    // fetch("/api/stocks")
+    //   .then((res) => res.json())
+    //   .then((data) => setStocks(data))
+    //   .catch((error) => console.error("Error fetching stock data:", error));
+    const fetchStockData = async () => {
+      try {
+        const response = await fetch("/api/stocks");
+        if (!response.ok) throw new Error("Error fetching stock data");
+
+        const data = await response.json();
+        console.log(data);
+
+        setStocks(data);
+      } catch (err) {
+        setError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStockData();
+  }, []);
+
   if (loading) return <p>Loading...</p>;
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -87,16 +130,23 @@ const InvestmentsPage: React.FC = () => {
       <ul>
         {investments.map((inv) => (
           <span className="grid md:grid-cols-2 lg:grid-cols-3" key={inv._id}>
-            <span className=" ">
-              {inv.type}: <span className=" font-serif">₹</span>
-              {inv.amount} on {new Date(inv.date).toLocaleDateString()}
-            </span>
-            <button
-              className=" text-left hover:underline text-red-600"
-              onClick={() => deleteInvestment(inv._id)}
-            >
-              Delete Investment
-            </button>
+            {inv.type !== "stock" ? (
+              <>
+                <span className=" ">
+                  {inv.type}: <span className=" font-serif">₹</span>
+                  {inv.amount} on{" "}
+                  {new Date(inv.date).toLocaleDateString("en-GB")}
+                </span>
+                <button
+                  className=" text-left hover:underline text-red-600"
+                  onClick={() => deleteInvestment(inv._id)}
+                >
+                  Delete Investment
+                </button>
+              </>
+            ) : (
+              ""
+            )}
           </span>
         ))}
       </ul>
@@ -141,6 +191,56 @@ const InvestmentsPage: React.FC = () => {
           <button type="submit">Add Investment</button>
         </div>
       </form>
+      <div>
+        {stockData.length != 0 ? (
+          <>
+            <h2 className="pt-5 font-extrabold text-xl">
+              Stock Portfolio(Only US stock exchange)
+            </h2>
+            <ul>
+              {stockData.map((stock, index) => (
+                <li className="pt-3" key={index}>
+                  <h2 className=" font-extrabold text-[20px]">
+                    {stock?.name}:
+                  </h2>
+                  <p className=" font-medium">
+                    Amount hold: <span className=" font-serif">₹</span>
+                    {stock?.amount} on{" "}
+                    {new Date(stock?.inv_date).toLocaleDateString("en-GB")}
+                  </p>
+                  <p className="font-medium">Status: {stock?.status}</p>
+                  {stock?.c ||
+                  stock?.h ||
+                  stock?.l ||
+                  stock?.o ||
+                  stock?.pc != 0 ? (
+                    <>
+                      <p>Current Price: ${stock?.c}</p>
+                      <p>Highest Price: ${stock?.h}</p>
+                      <p>Lowest Price: ${stock?.l}</p>
+                      <p>Opening: ${stock?.o}</p>
+                      <p>Previous Close: ${stock?.pc}</p>
+                    </>
+                  ) : (
+                    <>
+                      <p>Not in US Stock Exchange</p>
+                      <Link
+                        className="text-blue-600 font-medium hover:underline"
+                        href={`https://www.google.com/search?q=${stock?.name}+stock`}
+                        target="_blank"
+                      >
+                        Track Here
+                      </Link>
+                    </>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          ""
+        )}
+      </div>
     </section>
   );
 };
