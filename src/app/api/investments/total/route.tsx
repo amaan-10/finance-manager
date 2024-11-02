3df02e5e1cb1d3e2719ca20f86c98b7c62ad1946
Amaan <amaanshaikh.gg@gmail.com>
2024-11-02 23:13:26 +0530

    initial commit

---
+++ src/app/api/investments/total/route.tsx
@@ -0,0 +1,57 @@
+// app/api/budgets/monthly/[userId]/route.ts
+import { NextRequest, NextResponse } from "next/server";
+import Expense from "../../../models/Expense";
+import clientPromise from "../../../../lib/mongodb";
+import { connectToDatabase } from "@/lib/mongoose";
+import { getAuth } from "@clerk/nextjs/server";
+import InvestmentModel from "@/app/models/Investment";
+
+export async function GET(req: NextRequest) {
+  await connectToDatabase(); // Ensure MongoDB is connected
+  const { userId } = getAuth(req); // Get the authenticated user ID
+  const client = await clientPromise;
+
+  try {
+    // Find the user's budget for the current month and year
+    const investment = await InvestmentModel.findOne({
+      userId,
+    });
+
+    if (!investment) {
+      return NextResponse.json(
+        { message: "No investments found." },
+        { status: 404 }
+      );
+    }
+
+    // Calculate total expenses for the current month
+    const totalInvestment = await InvestmentModel.aggregate([
+      {
+        $match: {
+          userId,
+        },
+      },
+      { $group: { _id: null, totalAmount: { $sum: "$amount" } } },
+    ]);
+
+    const totalInvestmentByType = await InvestmentModel.aggregate([
+      {
+        $match: {
+          userId,
+        },
+      },
+      { $group: { _id: "$type", totalAmount: { $sum: "$amount" } } },
+    ]);
+
+    return NextResponse.json({
+      investment: totalInvestment[0],
+      byType: totalInvestmentByType,
+    });
+  } catch (error) {
+    console.error("Error fetching total investments:", error);
+    return NextResponse.json(
+      { error: "Error fetching total investments" },
+      { status: 500 }
+    );
+  }
+}
