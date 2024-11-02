c640572530d1da10b0d4c767345778ae3f11fd7d
Amaan <amaanshaikh.gg@gmail.com>
2024-11-02 23:16:01 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import WavingHand from "../assets/Waving Hand.png";
+import AnalysisSVG from "../assets/analysis-animate.svg";
+import FinanceSVG from "../assets/finance-animate.svg";
+import DataSVG from "../assets/financial-data-animate.svg";
+import RevenueSVG from "../assets/revenue-animate.svg";
 
 export default async function Home() {
   const user = await currentUser();
@@ -16,6 +20,38 @@ export default async function Home() {
 
         <h2>Welcome to Personal Finance Manager</h2>
         <p>Track your expenses, create budgets, and manage your investments.</p>
+        <div className="flex flex-col px-5">
+          <Image
+            src={AnalysisSVG}
+            alt="Analysis SVG"
+            width={350}
+            height={350}
+            unoptimized // This ensures the GIF is not optimized, preserving the animation.
+          />
+          <Image
+            src={FinanceSVG}
+            alt="Finance SVG"
+            className=" self-end"
+            width={350}
+            height={350}
+            unoptimized // This ensures the GIF is not optimized, preserving the animation.
+          />
+          <Image
+            src={DataSVG}
+            alt="Financial Data SVG"
+            width={350}
+            height={350}
+            unoptimized // This ensures the GIF is not optimized, preserving the animation.
+          />
+          <Image
+            src={RevenueSVG}
+            alt="Revenue SVG"
+            className=" self-end"
+            width={350}
+            height={350}
+            unoptimized // This ensures the GIF is not optimized, preserving the animation.
+          />
+        </div>
       </section>
     </div>
   );
