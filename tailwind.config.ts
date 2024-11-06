3c17b9e0c969a8c18332fc10a12835fcb1bcb410
Amaan <amaanshaikh.gg@gmail.com>
2024-11-06 15:27:21 +0530

    updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         foreground: "var(--foreground)",
       },
     },
+    screens: {
+      // Custom breakpoints
+      xs: "340px", // Extra small devices
+      sm: "460px", // Small devices
+      md: "810px", // Medium devices (Tablets)
+      lg: "1080px", // Large devices (Laptops)
+      xl: "1280px", // Extra large devices (Desktops)
+      "2xl": "1600px", // Super wide screens
+
+      // Adding a custom "portrait mode" breakpoint
+      portrait: { raw: "(orientation: portrait)" },
+    },
   },
   plugins: [],
 };
