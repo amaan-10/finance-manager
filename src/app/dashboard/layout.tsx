4536efc4ab2f2070b79fb781fbaa596134be0144
Amaan <amaanshaikh.gg@gmail.com>
2024-11-06 16:06:54 +0530

    mobile responsive

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,24 +1,76 @@
 // app/dashboard/layout.tsx
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const headers = new Headers(); // Placeholder: Replace with actual headers if needed
+
+  const pathname = usePathname();
+  const NavLink = ({
+    href,
+    children,
+    className,
+  }: {
+    className: string;
+    href: string;
+    children: ReactNode;
+  }) => {
+    const isActive = pathname === href;
+
+    return (
+      <Link
+        href={href}
+        className={`${
+          isActive ? " underline-offset-[1.5px] underline " : ""
+        }${className}`}
+      >
+        {children}
+      </Link>
+    );
+  };
   return (
     <div>
-      <nav>
-        <a className=" text-black" href="/dashboard">
-          Overview
-        </a>
-        <a className=" text-black" href="/dashboard/expenses">
-          Expenses
-        </a>
-        <a className=" text-black" href="/dashboard/budgets">
-          Budgets
-        </a>
-        <a className=" text-black" href="/dashboard/investments">
-          Investments
-        </a>
+      <nav className=" flex-col inline-flex sm:flex-row">
+        <span>
+          <span className="inline sm:hidden">- </span>
+          <NavLink className=" text-black" href="/dashboard">
+            Overview
+          </NavLink>
+        </span>
+        <span>
+          <span className="inline sm:hidden">- </span>
+          <NavLink
+            className=" mt-[2px] sm:mt-0 text-black"
+            href="/dashboard/expenses"
+          >
+            Expenses
+          </NavLink>
+        </span>
+        <span>
+          <span className="inline sm:hidden">- </span>
+          <NavLink
+            className=" mt-[2px] sm:mt-0 text-black"
+            href="/dashboard/budgets"
+          >
+            Budgets
+          </NavLink>
+        </span>
+        <span>
+          <span className="inline sm:hidden">- </span>
+          <NavLink
+            className=" mt-[2px] sm:mt-0 text-black"
+            href="/dashboard/investments"
+          >
+            Investments
+          </NavLink>
+        </span>
       </nav>
       <div>{children}</div>
     </div>
