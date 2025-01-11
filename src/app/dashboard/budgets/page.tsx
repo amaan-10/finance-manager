6af3f2f4791ccd6386fcd5ee75dd0bf2ad181876
Amaan <amaanshaikh.gg@gmail.com>
2025-01-11 22:12:10 +0530

    updated css

--- src/app/dashboard/budgets/page.tsx
+++ src/app/dashboard/budgets/page.tsx
@@ -196,7 +196,7 @@ export default function BudgetsPage() {
         </div>
         <button
           type="button"
-          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+          className=" border  focus:outline-none  focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 me-2 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700"
           onClick={handleAddBudget}
         >
           Add Budget
