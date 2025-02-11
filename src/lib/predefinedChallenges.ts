604cb9fd8b41c85805fb8a07b527e399686c2459
Amaan <amaanshaikh.gg@gmail.com>
2025-02-11 22:54:44 +0530

    initial commit

---
+++ src/lib/predefinedChallenges.ts
@@ -0,0 +1,6 @@
+export const predefinedChallenges = [
+    { id: "1", name: "No Coffee for a Week", goal: 7, badgeReward: "☕🚫 Caffeine-Free Champ" },
+    { id: "2", name: "Save ₹100 Every Day", goal: 30, badgeReward: "🏆 Savings Champion!" },
+    { id: "3", name: "Cancel One Subscription", goal: 1, badgeReward: "💡 Smart Saver" },
+  ];
+  
