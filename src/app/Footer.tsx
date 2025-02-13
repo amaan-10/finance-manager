de80a42356de5f710a063c573f95f90f27b6a588
Amaan <amaanshaikh.gg@gmail.com>
2025-02-13 23:03:57 +0530

    fix: component

---
+++ src/app/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className=" px-10 py-10 text-sm sm:text-base text-gray-500">
+        © 2025 SpendLess | Personal Finance Manager
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
