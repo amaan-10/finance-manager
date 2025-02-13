7d6e5f2577f2ba8303ae6ab11c1660d014426ff7
Amaan <amaanshaikh.gg@gmail.com>
2025-02-13 23:03:32 +0530

    fix: component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { IBM_Plex_Mono, Courier_Prime, Roboto_Mono } from "next/font/google";
 import { ReactNode } from "react";
 import Navbar from "./Navbar";
+import { LoaderProvider } from "./../context/LoaderContext";
+import Footer from "./Footer";
 
 const ibm = IBM_Plex_Mono({
   subsets: ["latin"],
@@ -39,27 +41,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${roboto.className}`}>
-          <header>
-            <nav>
-              <div className="p-5 flex items-center font-extrabold">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className=" h-[30px] w-[30px] font-normal"
-                  viewBox="0 0 24 24"
-                  fill="white"
-                >
-                  <path d="m6,9c-3.421,0-6,1.505-6,3.5v8c0,1.995,2.579,3.5,6,3.5s6-1.505,6-3.5v-8c0-1.995-2.579-3.5-6-3.5Zm4,7.5c0,.529-1.519,1.5-4,1.5s-4-.971-4-1.5v-1.348c1.046.533,2.435.848,4,.848s2.954-.315,4-.848v1.348Zm-4-5.5c2.481,0,4,.971,4,1.5s-1.519,1.5-4,1.5-4-.971-4-1.5,1.519-1.5,4-1.5Zm0,11c-2.481,0-4-.971-4-1.5v-1.348c1.046.533,2.435.848,4,.848s2.954-.315,4-.848v1.348c0,.529-1.519,1.5-4,1.5ZM24,5v14c0,2.757-2.243,5-5,5h-5c-.553,0-1-.448-1-1s.447-1,1-1h5c1.654,0,3-1.346,3-3V5c0-1.654-1.346-3-3-3h-10c-1.654,0-3,1.346-3,3v1c0,.552-.447,1-1,1s-1-.448-1-1v-1C4,2.243,6.243,0,9,0h10c2.757,0,5,2.243,5,5Zm-11,5c-.553,0-1-.448-1-1s.447-1,1-1h5v-2h-8v.5c0,.552-.447,1-1,1s-1-.448-1-1v-.5c0-1.103.897-2,2-2h8c1.103,0,2,.897,2,2v2c0,1.103-.897,2-2,2h-5Zm1,8c0-.552.447-1,1-1h4c.553,0,1,.448,1,1s-.447,1-1,1h-4c-.553,0-1-.448-1-1Zm0-4v-1c0-.552.447-1,1-1s1,.448,1,1v1c0,.552-.447,1-1,1s-1-.448-1-1Zm6,0c0,.552-.447,1-1,1s-1-.448-1-1v-1c0-.552.447-1,1-1s1,.448,1,1v1Z" />
-                </svg>
-
-                <h1 className=" pl-3 text-lg md:text-xl">SpendLess</h1>
-              </div>
-              <Navbar />
-            </nav>
-          </header>
-          <main className="px-10">{children}</main>
-          <footer className=" px-10 py-10 text-sm sm:text-base text-gray-500">
-            © 2025 SpendLess | Personal Finance Manager
-          </footer>
+          <LoaderProvider>
+            <Navbar />
+            <main className="px-10">{children}</main>
+            <Footer />
+          </LoaderProvider>
         </body>
       </html>
     </ClerkProvider>
