7fe88d8222bcf76391422fe823489cb387c278ff
Amaan <amaanshaikh.gg@gmail.com>
2025-03-25 23:38:22 +0530

    update: POST api

--- src/app/api/challenges/route.tsx
+++ src/app/api/challenges/route.tsx
@@ -1,7 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/mongoose";
 import ChallengeModel from "@/app/models/Challenge";
 import { predefinedChallenges } from "@/lib/predefinedChallenges";
+import { getAuth } from "@clerk/nextjs/server";
 
 export async function GET() {
   await connectToDatabase(); // Ensure MongoDB is connected
@@ -18,17 +19,62 @@ export async function GET() {
   return NextResponse.json(mergedChallenges, { status: 200 });
 }
 
-export async function POST(req: Request) {
-  try {
-    const { name, goal, userId } = await req.json();
-    await connectToDatabase(); // Ensure MongoDB is connected
-    const newChallenge = new ChallengeModel({ name, goal, userId });
-    await newChallenge.save();
+// export async function POST(req: Request) {
+//   try {
+//     const { name, goal, userId } = await req.json();
+//     await connectToDatabase(); // Ensure MongoDB is connected
+//     const newChallenge = new ChallengeModel({ name, goal, userId });
+//     await newChallenge.save();
 
-    return NextResponse.json(newChallenge, { status: 201 });
+//     return NextResponse.json(newChallenge, { status: 201 });
+//   } catch (error) {
+//     return NextResponse.json(
+//       { error: "Failed to create challenge" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+export async function POST(req: NextRequest) {
+  try {
+    const {
+      id,
+      title,
+      description,
+      icon,
+      total,
+      daysLeft,
+      difficulty,
+      category,
+      points,
+    } = await req.json();
+
+    const { userId } = getAuth(req);
+    if (!userId)
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const challenge = await ChallengeModel.create({
+      id,
+      title,
+      description,
+      icon,
+      total,
+      daysLeft,
+      difficulty,
+      category,
+      points,
+      userId: userId,
+      progress: 0,
+      isCompleted: false,
+      isClaimed: false,
+      streak: 0,
+    });
+
+    return NextResponse.json(challenge, { status: 201 });
   } catch (error) {
+    console.error("Error starting challenge:", error);
     return NextResponse.json(
-      { error: "Failed to create challenge" },
+      { error: "Failed to start challenge" },
       { status: 500 }
     );
   }
