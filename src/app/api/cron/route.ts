f7b3882853b25cfd8ab70409d94f9439f9ee63ec
Amaan <amaanshaikh.gg@gmail.com>
2025-03-25 23:39:40 +0530

    new: cron api

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,20 @@
+import ChallengeModel from "@/app/models/Challenge";
+import { connectToDatabase } from "@/lib/mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await connectToDatabase();
+
+  try {
+    const result = await ChallengeModel.updateMany(
+      { daysLeft: { $gt: 0 } },
+      { $inc: { daysLeft: -1 } }
+    );
+
+    console.log(`Updated ${result.modifiedCount} challenges`);
+    return NextResponse.json({ message: "Challenges updated" });
+  } catch (error) {
+    console.error("Error updating challenges:", error);
+    return NextResponse.json({ error: "Failed to update" }, { status: 500 });
+  }
+}
