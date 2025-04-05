36ab2a1eed1b342b036aae75c1ee5f87286bdcd4
Amaan <amaanshaikh.gg@gmail.com>
2025-04-05 21:52:26 +0530

    fix: error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/','/sign-in(.*)', '/sign-up(.*)', "/api/cron"])
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/api/cron',
+]);
 
-export default clerkMiddleware(
-  async (auth, request) => {
+export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect()
+    await auth.protect();
   }
- }
-
-)
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)', "/((?!api/cron).*)"
+    // Skip static files, Next.js internals, and allow /api/cron to bypass
+    '/((?!_next|.*\\.(?:html?|css|js|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|json)|api/cron).*)',
+    // Always run for API and TRPC routes (excluding /api/cron from above)
+    '/(api|trpc)(.*)',
   ],
 };
