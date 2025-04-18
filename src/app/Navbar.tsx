c4daf0a6054e6cb3ba33a1c13cd203ebc0625f3e
Amaan <amaanshaikh.gg@gmail.com>
2025-04-18 17:33:41 +0530

    fix: nav links

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -57,7 +57,10 @@ export default function Navbar() {
     <>
       <header className="absolute top-[50px] w-full px-5 z-50">
         <nav className="px-5 flex justify-between">
-          <div className="flex items-center font-extrabold">
+          <Link
+            href={"/"}
+            className="flex items-center font-extrabold text-black"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-7 w-7 font-normal"
@@ -67,7 +70,7 @@ export default function Navbar() {
               <path d="m6,9c-3.421,0-6,1.505-6,3.5v8c0,1.995,2.579,3.5,6,3.5s6-1.505,6-3.5v-8c0-1.995-2.579-3.5-6-3.5Zm4,7.5c0,.529-1.519,1.5-4,1.5s-4-.971-4-1.5v-1.348c1.046.533,2.435.848,4,.848s2.954-.315,4-.848v1.348Zm-4-5.5c2.481,0,4,.971,4,1.5s-1.519,1.5-4,1.5-4-.971-4-1.5,1.519-1.5,4-1.5Zm0,11c-2.481,0-4-.971-4-1.5v-1.348c1.046.533,2.435.848,4,.848s2.954-.315,4-.848v1.348c0,.529-1.519,1.5-4,1.5ZM24,5v14c0,2.757-2.243,5-5,5h-5c-.553,0-1-.448-1-1s.447-1,1-1h5c1.654,0,3-1.346,3-3V5c0-1.654-1.346-3-3-3h-10c-1.654,0-3,1.346-3,3v1c0,.552-.447,1-1,1s-1-.448-1-1v-1C4,2.243,6.243,0,9,0h10c2.757,0,5,2.243,5,5Zm-11,5c-.553,0-1-.448-1-1s.447-1,1-1h5v-2h-8v.5c0,.552-.447,1-1,1s-1-.448-1-1v-.5c0-1.103.897-2,2-2h8c1.103,0,2,.897,2,2v2c0,1.103-.897,2-2,2h-5Zm1,8c0-.552.447-1,1-1h4c.553,0,1,.448,1,1s-.447,1-1,1h-4c-.553,0-1-.448-1-1Zm0-4v-1c0-.552.447-1,1-1s1,.448,1,1v1c0,.552-.447,1-1,1s-1-.448-1-1Zm6,0c0,.552-.447,1-1,1s-1-.448-1-1v-1c0-.552.447-1,1-1s1,.448,1,1v1Z" />
             </svg>
             <h1 className=" pl-3 text-lg md:text-[22px]">SpendLess</h1>
-          </div>
+          </Link>
           <div className="flex justify-between items-center text-sm md:text-base">
             <span className="mb-0 my-auto inline-flex md:inline sign-in-container">
               <SignedOut>
